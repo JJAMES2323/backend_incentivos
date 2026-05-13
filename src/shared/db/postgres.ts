175f24b5a008ec0e89f8cb0900af2181a63c08bc
jamesor2017 <jamesor2017@gmail.com>
2026-05-12 22:51:40 -0500

    fix: use connection string for supabase

--- src/shared/db/postgres.ts
+++ src/shared/db/postgres.ts
@@ -1,14 +1,9 @@
 import { Pool } from 'pg';
 import { env } from '../../config/env';
 
-export const pool = new Pool ({
-    host: env.DB_HOST,
-    port: Number(env.DB_PORT),
-    user: env.DB_USER,
-    password: env.DB_PASSWORD,
-    database: env.DB_NAME,
-    ssl: { rejectUnauthorized: false },
-    family: 4,
+export const pool = new Pool({
+  connectionString: `postgresql://${env.DB_USER}:${encodeURIComponent(env.DB_PASSWORD)}@${env.DB_HOST}:${Number(env.DB_PORT)}/${env.DB_NAME}?sslmode=require`,
+  ssl: { rejectUnauthorized: false },
 });
 
 pool.query('SELECT 1')
