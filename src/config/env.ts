8666263e4140e7cd6b28fc8530c000cd13e25ce8
jamesor2017 <jamesor2017@gmail.com>
2026-05-13 18:59:03 -0500

    feat: use DATABASE_URL env var

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,11 +4,8 @@ dotenv.config();
 
 export const env = {
     PORT: process.env.PORT!,
-    DB_HOST: process.env.DB_HOST!,
-    DB_PORT: Number(process.env.DB_PORT)!,
-    DB_USER: process.env.DB_USER!,
-    DB_PASSWORD: process.env.DB_PASSWORD!,
-    DB_NAME: process.env.DB_NAME!,
+
+    DATABASE_URL: process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
 
     JWT_SECRET: process.env.JWT_SECRET || 'secret',
 }
--- src/shared/db/postgres.ts
+++ src/shared/db/postgres.ts
@@ -2,11 +2,7 @@ import { Pool } from 'pg';
 import { env } from '../../config/env';
 
 export const pool = new Pool({
-  host: env.DB_HOST,
-  port: Number(env.DB_PORT),
-  user: env.DB_USER,
-  password: env.DB_PASSWORD,
-  database: env.DB_NAME,
+  connectionString: env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
